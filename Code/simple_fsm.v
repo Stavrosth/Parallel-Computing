0b6bd8a8a2f3662028d982ba7127f0dedc69e543
Stavrosth <128798689+Stavrosth@users.noreply.github.com>
2025-05-27 20:36:00 +0300

    Added constraints for the implementation. Behavioural Simulation works. Post synthesis Functional simulation works except for main branch pc. Post synthesis Timing simulation oute gia plaka. Added Comments and made the code a little more beautiful.

--- Code/simple_fsm.v
+++ Code/simple_fsm.v
@@ -11,41 +11,42 @@ module simpleFSM(
     out_instruction
     );
     
-    input clk;
-    input reset;
-    input [31:0] instruction; //32-bit instruction from IF/ID
-    input [31:0] immediate; //immediate value from IF/ID
-    input [31:0] curr_PC;
-    input mispredict;
-    output reg block_signal;
-    output reg flush;
+    /***** INPUTS & OUTPUTS******/
+    input clk, reset, mispredict;
+    input [31:0] immediate, instruction, curr_PC;
+    output reg block_signal, flush;
     output reg [31:0] new_pc;
-    reg [1:0] current_state, next_state;
-    reg [31:0] main_branch_pc;
-    reg change_main_pc;
-    reg read_enable, write_enable;
+
+    /***** FSM VARIABLES ******/
+    reg [2:0] current_state, next_state;
+    reg [31:0] main_branch_pc; // Program Counter of the branch
+    reg change_main_pc; // Allows the FSM to change the main branch pc
+    reg [31:0] reg_immediate; // Was added because the synthesis was not working
+    reg [31:0] pc_to_store; // Stores Temporarily the PC of the branch to store in the next cycle in main_branch_pc
+    reg [31:0] branch_immediate; // Used to calculate the read address of the bram during reuse
+    reg [6:0] opcode; // Was added because the synthesis was not working
+    reg mispredict_signal; // Was added because the synthesis was not working
+    reg must_reset; // Was added because the synthesis was not working (for the FSM to return to RESET state)
+    reg goto_buffering; // Detects a branch
+    reg change_address; // Allows the bram to change address
+    wire change_address_first; // Allows Bram to change address one cycle earlier in order to store the first instruction
+    parameter signed [31:0] LOOP_SIZE = -27; // MMAXIMUM LOOP SIZE accepted 
+
+    parameter TRACK = 3'b000,    // State encoding for the FSM
+              BUFFERING = 3'b001,
+              WAIT = 3'b010, 
+              REUSE = 3'b011, 
+              RESET = 3'b100;
+
+    parameter JAL_OPCODE =  7'b1101111, // remove this when implemented to RISC V 
+              BTYPE_OPCODE = 7'b1100011; // then we will get the opcode from the include file
+              
+    /***** BRAM VARIABLES ******/    
     output [31:0] out_instruction;
-    
     reg [5:0] write_address, read_address;
+    reg read_enable, write_enable;
 
-    wire [6:0] opcode;
-
-    wire mispredict_signal = mispredict;
-    reg must_reset;
-    wire goto_buffering;
-    
-    parameter TRACK = 2'b00,    
-              BUFFERING = 2'b01, 
-              REUSE = 2'b10, 
-              RESET = 2'b11;
-
-    /*** remove this when implemented to RISC V ***/
-    //then we will get the opcode from the include file 
-    parameter JAL_OPCODE =  7'b1101111,
-              BTYPE_OPCODE = 7'b1100011;
-
-    parameter signed [31:0] LOOP_SIZE = -27;
-
+    // FSM sequential block
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             current_state <= TRACK;
@@ -54,95 +55,136 @@ module simpleFSM(
         end
     end
 
+    // FSM combinational block
     always @(*) begin
         next_state = current_state;
         block_signal = 1'b0;
         flush = 1'b0;
         read_enable = 1'b0;
         write_enable = 1'b0;
+        change_main_pc = 1'b0;
+        change_address = 1'b0;
         
         case (current_state)
             TRACK: begin
-               if(goto_buffering) begin
-                    change_main_pc = 1'b1;
-                    write_enable = 1'b1;
+                write_enable = 1'b1;
+
+               if(goto_buffering) begin // Detects a branch
+                    change_address = 1'b1;
                     next_state = BUFFERING;
                 end else begin
-                    change_main_pc = 1'b0;
+                    change_main_pc = 1'b1;
                     next_state = TRACK;
                 end
             end
             BUFFERING: begin
-                change_main_pc = 1'b1;
-                // write_enable = 1'b1;
-                if(opcode == BTYPE_OPCODE || opcode == JAL_OPCODE) begin //check that loop is a basic block
+                if(opcode== BTYPE_OPCODE || opcode == JAL_OPCODE) begin //check that loop is a basic block
                     if (must_reset) begin // make sure the branch is not the one of our loop
                         next_state = RESET;
                     end
                     else begin
                         block_signal = 1'b1;
-
-                        next_state = REUSE;
+                        next_state = WAIT;
                     end
                 end
                 else begin
+                    change_address = 1'b1;
                     write_enable = 1'b1;
                     next_state = BUFFERING;
                 end
             end
-            REUSE: begin
-                change_main_pc = 1'b1;
+            WAIT: begin // Waits for BRAM to read
                 block_signal = 1'b1;
+                read_enable = 1'b1;
+                next_state = REUSE;
+            end
+            REUSE: begin
                 if(mispredict_signal == 1'b1) begin
                     flush = 1'b1;
                     next_state = RESET;
                 end else begin
+                    block_signal = 1'b1;
                     read_enable = 1'b1;
                     next_state = REUSE;
                 end
             end
-            RESET: begin
-                flush = 1'b1;
-                change_main_pc = 1'b0;
+            RESET: begin /****** MAY BE REMOVED ******/
                 next_state = TRACK;
             end
             default: begin 
-                change_main_pc = 1'b0;
+                next_state = current_state;
+                block_signal = 1'b0;
                 flush = 1'b0;
-                next_state = TRACK;
+                read_enable = 1'b0;
+                write_enable = 1'b0;
+                change_main_pc = 1'b0;
+                change_address = 1'b0;
             end
         endcase
+
     end
 
-    // In case we detect a subloop
-    assign goto_buffering = ((opcode == BTYPE_OPCODE || opcode == JAL_OPCODE) && (immediate[31] == 1'b1) && (immediate >= LOOP_SIZE)) ? 1'b1 : 1'b0;
-    assign opcode = instruction[6:0];
-
-    always @(posedge clk) begin
-        if(change_main_pc == 1'b0) begin
-            main_branch_pc <= curr_PC;
-        end
-
-        if (main_branch_pc != curr_PC) begin
-            must_reset <= 1'b1;
+    // Was added because the synthesis was not working
+    always@(posedge clk or posedge reset) begin
+        if(reset) begin
+            opcode<=0;
+            reg_immediate <= 0;
+            mispredict_signal <= 0;
         end else begin
-            must_reset <= 1'b0;
+            opcode<=instruction[6:0];
+            reg_immediate <= immediate;
+            mispredict_signal <= mispredict;
         end
+    end
+    
+    // Always block that manages the pc_to_store, goto_buffering and branch_immediate
+    always@(posedge clk or posedge reset) begin   
+        if(reset) begin
+            pc_to_store <= 32'd0;
+            goto_buffering<=1'b0;
+            branch_immediate <= 32'd0;
+        end else if(((opcode == BTYPE_OPCODE || opcode == JAL_OPCODE) && (reg_immediate[31] == 1'b1) && (reg_immediate >= LOOP_SIZE))) begin
+            pc_to_store <= 32'd0;
+            goto_buffering<=1'b1;
+            branch_immediate<= reg_immediate;
+        end else begin
+            pc_to_store <= curr_PC;
+            goto_buffering<=1'b0;
+        end
+    end
 
-        if (flush == 1'b1) begin
+    //Always block that manages the main branch pc and new pc
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            main_branch_pc <= 32'd0;
+            new_pc <= 32'd0;
+        end else if(change_main_pc == 1'b1) begin
+            main_branch_pc <= pc_to_store;
+        end else if (flush == 1'b0) begin
             new_pc <= main_branch_pc + 4;
         end
     end
 
+    //Always block that manages the must_reset signal
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            must_reset <= 1'b0;
+        end else if (main_branch_pc == curr_PC) begin
+            must_reset <= 1'b0;
+        end else if (main_branch_pc != curr_PC) begin
+            must_reset <= 1'b1;
+        end
+    end
+
+    //Always block that manages read signals for bram
     always @(posedge clk or posedge reset) begin
         if(reset == 1'b1) begin
             read_address <= 6'd0;
         end else if(read_enable == 1'b0) begin
-            read_address <= 6'd0;
+            read_address <= 6'b0;
         end else begin
-            if ((read_address>>3) >= (~immediate +1)) begin
-                // $display("innn\n");
-                read_address <= 6'd0;
+            if ((read_address>>3) >= (~branch_immediate +1)) begin
+                read_address <= 6'h0;
             end else begin
                 read_address <= read_address + 6'd8;
             end
@@ -153,12 +195,15 @@ module simpleFSM(
     always @(posedge clk or posedge reset) begin
         if(reset == 1'b1) begin
             write_address <= 6'd0;
-        end else if(write_enable == 1'b1) begin
+        end else if((write_enable == 1'b1 && change_address == 1'b1) || change_address_first == 1'b1) begin
             write_address <= write_address + 6'd8;
         end else begin
             write_address <= 6'd0;
         end
     end
+
+    // Used to Allow the BRAM to write the first instruction of the loop
+    assign change_address_first = ((opcode == BTYPE_OPCODE || opcode == JAL_OPCODE) && (reg_immediate[31] == 1'b1) && (reg_immediate >= LOOP_SIZE)) ? 1'b1 : 1'b0;
 
     uop_cache bram(.clk(clk), .reset(reset), .instruction(instruction), .read_enable(read_enable), .write_enable(write_enable), .read_address(read_address), .write_address(write_address), .out_instruction(out_instruction));
 
--- Code/simple_testbench.v
+++ Code/simple_testbench.v
@@ -31,9 +31,10 @@ module FSM_testbench;
         clk=1'b1;
         reset=1'b0;
         immediate=32'b0;
+        mispredict = 1'b0;
         #10 reset = 1'b1;
         #10 reset = 1'b0;
-
+// #1000
         // Test cases can be added here
         // For example, you can toggle the reset signal and observe the FSM behavior
 
@@ -48,26 +49,31 @@ module FSM_testbench;
         //         #10 mispredict = 1'b0;
         //     end
        // end
-
-        #10 curr_PC = 32'h00000100; instruction = 32'h00000013; // NOP at 0x100
-        #10 curr_PC = 32'h00000104; instruction = 32'h00000014; // NOP at 0x104
-        #10 curr_PC = 32'h00000108; instruction = 32'h00000015; // NOP at 0x108
+       
+        #10 curr_PC = 32'h00000100; instruction = 32'h00000013; immediate = -0;// NOP at 0x100
+        #10 curr_PC = 32'h00000104; instruction = 32'h00000014; immediate = -0;// NOP at 0x104
+        #10 curr_PC = 32'h00000108; instruction = 32'h00000015; immediate = -0;// NOP at 0x108
         #10 curr_PC = 32'h0000011C; instruction = 32'hFC000AE3; immediate = -3;
-        #10 curr_PC = 32'h00000100; instruction = 32'h00000013; // NOP at 0x100
-        #10 curr_PC = 32'h00000104; instruction = 32'h00000014; // NOP at 0x104
-        #10 curr_PC = 32'h00000108; instruction = 32'h00000015; // NOP at 0x108
+        #10 curr_PC = 32'h00000100; instruction = 32'h00000013; immediate = -0;// NOP at 0x100
+        #10 curr_PC = 32'h00000104; instruction = 32'h00000014; immediate = -0;// NOP at 0x104
+        #10 curr_PC = 32'h00000108; instruction = 32'h00000015; immediate = -0;// NOP at 0x108
         #10 curr_PC = 32'h0000011C; instruction = 32'hFC000AE3; immediate = -3;
-        #10 instruction = 32'bz;// immediate=32'b0;// NOP at 0x100
+        #10 curr_PC = 32'h00000100; instruction = 32'h00000013; immediate = -0;// NOP at 0x100
+        #10 curr_PC = 32'h00000104; instruction = 32'h00000014; immediate = -0;// NOP at 0x104
+        #10 curr_PC = 32'h00000108; instruction = 32'h00000015; immediate = -0;// NOP at 0x108
+        #10 curr_PC = 32'h0000011C; instruction = 32'hFC000AE3; immediate = -3;
+        #10 instruction = 32'b0;// immediate=32'b0;// NOP at 0x100
+        
         #150 mispredict = 1'b1; // Wait for some time before the next iteration
-        #10 curr_PC = 32'h00000110; instruction = 32'h00000016; mispredict = 1'b0; // NOP at 0x100
-        #10 curr_PC = 32'h00000114; instruction = 32'h00000017; // NOP at 0x104
-        #10 curr_PC = 32'h00000118; instruction = 32'h00000018; // NOP at 0x108
+        #10 curr_PC = 32'h00000110; instruction = 32'h00000016; immediate = -0; mispredict = 1'b0; // NOP at 0x100
+        #10 curr_PC = 32'h00000114; instruction = 32'h00000017; immediate = -0;// NOP at 0x104
+        #10 curr_PC = 32'h00000118; instruction = 32'h00000018; immediate = -0;// NOP at 0x108
         #10 curr_PC = 32'h0000010C; instruction = 32'hFC000AE3; immediate = -3;
-        #10 curr_PC = 32'h00000110; instruction = 32'h00000016; // NOP at 0x100
-        #10 curr_PC = 32'h00000114; instruction = 32'h00000017; // NOP at 0x104
-        #10 curr_PC = 32'h00000118; instruction = 32'h00000018; // NOP at 0x108
+        #10 curr_PC = 32'h00000110; instruction = 32'h00000016; immediate = -0;// NOP at 0x100
+        #10 curr_PC = 32'h00000114; instruction = 32'h00000017; immediate = -0;// NOP at 0x104
+        #10 curr_PC = 32'h00000118; instruction = 32'h00000018; immediate = -0;// NOP at 0x108
         #10 curr_PC = 32'h0000010C; instruction = 32'hFC000AE3; immediate = -3; 
-
+        #10 curr_PC = 32'h0; instruction = 32'h0; immediate = 0;// NOP at 0x100
         #500 $finish; // End simulation after some time
     end
 
